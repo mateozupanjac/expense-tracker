8c10733517f25125da079f10c092651b8fa44e2b
Mateo Županjac <zupa93@windowslive.com>
2025-06-30 22:27:42 +0200

    init

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,18 +2,10 @@ import { StyleSheet, Text } from 'react-native';
 
 export default function HomeScreen() {
   return (
-<<<<<<< HEAD
   <Text>HELLO WORLD!</Text>
-=======
-   <Text>HELLO WORLD!</Text>
->>>>>>> d718f0b (init/ET-01 Init Expense Tracker project)
   );
 }
 
 const styles = StyleSheet.create({
-<<<<<<< HEAD
   
-=======
- 
->>>>>>> d718f0b (init/ET-01 Init Expense Tracker project)
 });
